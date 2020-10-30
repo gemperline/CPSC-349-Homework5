6dfb6e405f7556012d9ebbdb259d55bc96d14726
gemperline <39207952+gemperline@users.noreply.github.com>
2020-10-29 23:55:14 -0700

    firestore key added

--- coffeerun/functions/index.js
+++ coffeerun/functions/index.js
@@ -1,4 +1,12 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:/Users/Adam/iCloudDrive/Documents/School/Computer Science/349/coffee-run-b6d38-firebase-adminsdk-sfvqw-5ef5590f36.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coffee-run-b6d38.firebaseio.com"
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
